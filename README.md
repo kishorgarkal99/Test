7d32acee8d24dde17a807a6ec7e7bec5eb9b52e4
Abhishek Shukla <85979695+KingMaker960@users.noreply.github.com>
2022-11-08 13:15:26 +0545

    Encryption

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# tesst
+# metamask
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,194 +1,10 @@
+import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:convert/convert.dart';
-import 'package:web3dart/credentials.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:walletconnect_dart/walletconnect_dart.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:flutter_js/flutter_js.dart';
-// import 'package:flutter_js/javascript_runtime.dart';
-// import 'package:graphql_flutter/graphql_flutter.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await initHiveForFlutter();
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter GraphQL Demo',
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key});
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   JavascriptRuntime flutterJs = getJavascriptRuntime();
-//   var data = [];
-
-// //read data
-//   fetchData() async {
-//     HttpLink link = HttpLink("https://rickandmortyapi.com/graphql");
-//     GraphQLClient qlient =
-//         GraphQLClient(link: link, cache: GraphQLCache(store: HiveStore()));
-//     QueryResult result =
-//         await qlient.query(QueryOptions(document: gql("""query {
-//     characters(){
-//       results {
-//         image
-//         name
-//       }
-//    }} """)));
-//     setState(() {
-//       data = result.data!['characters']['results'];
-//       isLoading = false;
-//     });
-//   }
-
-//   bool isLoading = true;
-//   TextEditingController nameController = TextEditingController();
-//   TextEditingController username = TextEditingController();
-//   TextEditingController number = TextEditingController();
-//   var _scaffoldKey = GlobalKey<ScaffoldState>();
-
-//   @override
-//   void initState() {
-//     fetchData();
-//     super.initState();
-//   }
-
-//   @override
-//   void dispose() {
-//     nameController.dispose();
-//     username.dispose();
-//     number.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         key: _scaffoldKey,
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () async {
-//             String blocJs = await rootBundle.loadString("asset/test.js");
-//             try {
-//               JsEvalResult jsResult = flutterJs.evaluate(blocJs);
-//               print(jsResult);
-//             } catch (exc) {
-//               print(exc);
-//             }
-
-//             // showDialog(
-//             //     context: _scaffoldKey.currentContext!,
-//             //     builder: (BuildContext ctxt) => dialog(context));
-//           },
-//           child: const Icon(Icons.add),
-//         ),
-//         appBar: AppBar(
-//           title: const Text("Test"),
-//         ),
-//         body: const Center(
-//           child: Text("Please Watch Console For Response Time"),
-//         ));
-//   }
-
-// //   Widget dialog(BuildContext context) {
-// //     return Dialog(
-// //       insetPadding: const EdgeInsets.all(15),
-// //       child: Padding(
-// //         padding: const EdgeInsets.all(15.0),
-// //         child: Column(
-// //           mainAxisSize: MainAxisSize.min,
-// //           children: [
-// //             const Text(
-// //               "Enter New Data To Add",
-// //               style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-// //             ),
-// //             const SizedBox(
-// //               height: 15,
-// //             ),
-// //             Container(
-// //               margin: const EdgeInsets.symmetric(horizontal: 25),
-// //               child: TextField(
-// //                 controller: nameController,
-// //                 decoration: InputDecoration(
-// //                     fillColor: Colors.white,
-// //                     filled: true,
-// //                     border: OutlineInputBorder(
-// //                         borderRadius: BorderRadius.circular(10)),
-// //                     hintText: "Enter Name",
-// //                     labelText: "Name"),
-// //               ),
-// //             ),
-// //             const SizedBox(
-// //               height: 20,
-// //             ),
-// //             Container(
-// //               margin: const EdgeInsets.symmetric(horizontal: 25),
-// //               child: TextField(
-// //                 controller: username,
-// //                 decoration: InputDecoration(
-// //                     fillColor: Colors.white,
-// //                     filled: true,
-// //                     border: OutlineInputBorder(
-// //                         borderRadius: BorderRadius.circular(10)),
-// //                     hintText: "Enter Username",
-// //                     labelText: "Username"),
-// //               ),
-// //             ),
-// //             const SizedBox(
-// //               height: 20,
-// //             ),
-// //             Container(
-// //               margin: const EdgeInsets.symmetric(horizontal: 25),
-// //               child: TextField(
-// //                 controller: number,
-// //                 keyboardType: TextInputType.phone,
-// //                 decoration: InputDecoration(
-// //                     fillColor: Colors.white,
-// //                     filled: true,
-// //                     border: OutlineInputBorder(
-// //                         borderRadius: BorderRadius.circular(10)),
-// //                     hintText: "Enter Number",
-// //                     labelText: "Number"),
-// //               ),
-// //             ),
-// //             const SizedBox(
-// //               height: 20,
-// //             ),
-// //             Align(
-// //               alignment: Alignment.bottomRight,
-// //               child: ElevatedButton(
-// //                 child: const Text("Save"),
-// //                 onPressed: () {
-// //                   Navigator.pop(context);
-// //                 },
-// //               ),
-// //             )
-// //           ],
-// //         ),
-// //       ),
-// //     );
-// //   }
-// }
 void main() {
   runApp(const MyApp());
 }
@@ -199,53 +15,107 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Metamask Integration',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MetamaskConnect(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class MetamaskConnect extends StatefulWidget {
+  const MetamaskConnect({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MetamaskConnect> createState() => _MetamaskConnectState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  bool isLoading = false;
-  String Data = "Processing...";
+class _MetamaskConnectState extends State<MetamaskConnect> {
+  var connector = WalletConnect(
+    bridge: "https://bridge.walletconnect.org",
+    clientMeta: const PeerMeta(
+      name: "Deme",
+      description: "NFT App",
+      url: "https://walletconnect.org",
+      icons: [
+        "https://files.gitbook.com/v0/b/gitbook-legacy-files/o/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media"
+      ],
+    ),
+  );
+
+  var _session, _uri, Data = "Please Click Play Button To Encrypt Message";
+  var userkey;
+
+  LoginUsingMetamask(BuildContext context) async {
+    if (!connector.connected) {
+      try {
+        var session = await connector.createSession(onDisplayUri: (uri) async {
+          _uri = uri;
+          await launchUrlString(uri, mode: LaunchMode.externalApplication);
+        });
+        setState(() {
+          _session = session;
+        });
+        // var key = await connector.sendCustomRequest(
+        //     method: 'eth_getEncryptionPublicKey',
+        //     params: [session.accounts[0].toString()]);
+        // setState(() {
+        //   userkey = key;
+        // });
+      } catch (error) {
+        print(error);
+      }
+    } else {
+      print("Connection Successful");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Test"),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            isLoading = true;
-          });
-          testerFunction();
+        onPressed: () async {
+          await testerFunction(WalletConnectCipher()
+              .generateKey(length: 32, random: Random.secure()));
         },
         child: const Icon(Icons.play_arrow),
       ),
-      body: Center(
-          child: Text(!isLoading ? "Enter Play Button To Start." : Data)),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SafeArea(
+            child: Center(
+          child: _session == null
+              ? ElevatedButton(
+                  onPressed: () async {
+                    //connect wallet here
+                    await LoginUsingMetamask(context);
+                  },
+                  child: const Text("Connect Wiith Metamask"))
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      ">>Account Connected \n >>Wallet ID: ${_session.accounts[0]}",
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      Data,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+        )),
+      ),
     );
   }
 
-  testerFunction() async {
-    var batchSize = 10;
-    List<EthPrivateKey> addressBatch = List.filled(
-      batchSize,
-      EthPrivateKey.createRandom(Random.secure()),
-      growable: true,
-    );
+  testerFunction(key) async {
+    var batchSize = 10000;
     random(length) {
       String chars;
       chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -260,37 +130,27 @@ class _MyHomePageState extends State<MyHomePage> {
     if (kDebugMode) {
       print("Processing.....");
     }
-    DateTime startTime = DateTime.now();
-    for (int i = 0; i < batchSize; i++) {
-      addressBatch[i] = EthPrivateKey.createRandom(Random.secure());
+    var startTime = DateTime.now();
+    try {
+      for (int i = 0; i < batchSize; i++) {
+        var secretMessage = random(1000);
+        await WalletConnectCipher()
+            .encrypt(data: ascii.encode(secretMessage), key: await key);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
     }
-    DateTime endTime = DateTime.now();
-    if (kDebugMode) {
-      print(
-          "Generated $batchSize addresses in ${endTime.difference(startTime).inMilliseconds} ms");
-    }
-    setState(() {
-      Data =
-          "Generated $batchSize addresses in ${endTime.difference(startTime).inMilliseconds} ms";
-    });
-    // for (int i = 0; i < batchSize; i++) {
-    //   print(
-    //       "u[$i]\naddress: ${addressBatch[i].address}\npublickey: ${hex.encode(addressBatch[0].encodedPublicKey)},\nprivateKey: ${hex.encode(addressBatch[0].privateKey)}");
-    // }
+    var endTime = DateTime.now();
 
-    //---------------------------------------------msg creation------------------------------------------------------
-    startTime = DateTime.now();
-    for (int i = 0; i < batchSize; i++) {
-      var secretMessage = random(1000);
-    }
-    endTime = DateTime.now();
     if (kDebugMode) {
       print(
-          "$Data\nEncrypted $batchSize messages in ${endTime.difference(startTime).inMilliseconds} ms");
+          "Encrypted $batchSize messages in ${endTime.difference(startTime).inMilliseconds} ms");
     }
     setState(() {
       Data =
-          "$Data\nEncrypted $batchSize messages in ${endTime.difference(startTime).inMilliseconds} ms";
+          "Encrypted $batchSize messages in ${endTime.difference(startTime).inMilliseconds} ms";
     });
   }
 }
